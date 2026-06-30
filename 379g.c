a87838a7c8b96ce2d47345ba4d3748d6a5b9326b
Yuri <glazunov2411@mail.ru>
2026-06-30 21:11:42 +0900

    Update

---
+++ 379g.c
@@ -0,0 +1,53 @@
+// Задача 379г
+// Дана действительная матрица размера n x m. Определить числа b1, ..., bm, равные соответственно
+// значениям средних арефметических элементов строк
+// https://ivtipm.github.io/Programming/Glava11/index11.htm#z379
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main() {
+    int n, m;
+    float sum = 0.0;
+    printf("Введите n: ");
+    scanf("%d", &n);
+    printf("Введите m: ");
+    scanf("%d", &m);
+
+    float matr[n][m];
+    float b[m];
+
+    srand(time(0));       // инициализация генератора случайных чисел
+
+    // заполнение матрицы случайными вещественными числами от -100 до 100
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            matr[i][j] = rand() * 200.0 / RAND_MAX - 100.0;        // RAND_MAX = 32767 (обычно)
+        }
+    }
+
+    for (int j = 0; j < m; j++) { 
+        sum = 0.0; 
+        for (int i = 0; i < n; i++) {
+            sum += matr[i][j];
+        }
+        b[j] = sum / n;
+    }
+
+    // Вывод сгенерированной матрицы
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%7.2f ", matr[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n Матрица b: ");
+    for (int i = 0; i < m; i++) {
+        printf("%.2f  ", b[i]);
+    }
+
+    return 0;
+}
---
+++ 698.c
@@ -0,0 +1,52 @@
+// Задача 698
+// Дана квадратная матрица порядка n. Получить матрицу A^2.
+// https://ivtipm.github.io/Programming/Glava21/index21.htm#z698
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int n;
+    printf("Введите n: ");
+    scanf("%d", &n);
+
+    float A[n][n], B[n][n];
+
+    srand(time(0));       // инициализация генератора случайных чисел
+
+    // заполнение матрицы случайными вещественными числами от -10 до 10
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            A[i][j] = rand() * 20.0 / RAND_MAX - 10.0;        // RAND_MAX = 32767 (обычно)
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            B[i][j] = 0;
+            for (int k = 0; k < n; k++) {
+                B[i][j] += A[i][k] * A[k][j];
+            }
+        }
+    }
+
+    // Вывод сгенерированной матрицы A
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%9.2f ", A[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Вывод результата
+    printf("\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%9.2f ", B[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
