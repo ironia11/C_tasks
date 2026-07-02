c830827210140738e557e38080e8623a08f30db2
Yuri <glazunov2411@mail.ru>
2026-07-03 01:12:09 +0900

    Update

--- task442.c
+++ task442.c
@@ -1,6 +1,6 @@
 // Задача 442
 // Даны действительные числа x1, y1, x2, y2, ..., x10, y10. Найти периметр десятиугольника,
-// вершины которого имеют соответственно координаты (x1, y1), ( x2, y2), ..., (x10, y10).
+// вершины которого имеют соответственно координаты (x1, y1), (x2, y2), ..., (x10, y10).
 // (Определить процедуру вычисления расстояния между двумя точками, заданными своими координатами).
 // https://ivtipm.github.io/Programming/Glava12/index12.htm#z442
 
@@ -8,32 +8,49 @@
 #include <math.h>
 #define N 10
 
-
-// функция для вычисления расстояния между двумя точками
-// x0, y0 - координаты первой точки, x, y - координаты второй точки
-// возвращает расстояние между двумя точками
-float distance(float x0, float y0, float x, float y) {
-    return sqrt(powf((x - x0), 2) + powf((y - y0), 2)); 
-}
+float distance(float x0, float y0, float x, float y);
+void input_coordinates(float X[100], float Y[100]);
+void calc_perimeter(float *perimeter, float X[], float Y[]);
+void print_result(float result);
 
 
 int main() {
     float X[100], Y[100];
-    float perimeter = 0;
+    float perim = 0.0;
+
+    input_coordinates(X, Y);
+    calc_perimeter(&perim, X, Y);
+    print_result(perim);
+
+    return 0;
+}
+
+
+// вычисления расстояния между двумя точками
+// x0, y0 - координаты первой точки, x, y - координаты второй точки
+float distance(float x0, float y0, float x, float y) {
+    return sqrt(powf((x - x0), 2) + powf((y - y0), 2)); 
+}
+
+// ввод координат
+void input_coordinates(float X[], float Y[]) {
     for (int i = 0; i < N; i++) {
         printf("Введите x[%d] и y[%d]: ", i + 1, i + 1);
         scanf("%f %f", &X[i], &Y[i]);
     }
+}
 
+// вычисление периметра
+void calc_perimeter(float *perimeter, float X[], float Y[]) {
     for (int i = 0; i < N; i++) {
-        if (i == 9) {
-            perimeter += distance(X[i], Y[i], X[0], Y[0]);
-            break;
-        }
-        perimeter += distance(X[i], Y[i], X[i+1], Y[i+1]);
-
+        if (i != N - 1)
+            *perimeter += distance(X[i], Y[i], X[i+1], Y[i+1]);
+        else 
+            *perimeter += distance(X[i], Y[i], X[0], Y[0]);
     }
+}
 
-    printf("%.3f", perimeter);
-    return 0;
+// вывод результата
+void print_result(float result) {
+    printf("%.3f", result);
 }
--- task692i.c
+++ task692i.c
@@ -3,6 +3,83 @@
 // расположенных в заштрихованной части матрицы (рис. 39).
 // https://ivtipm.github.io/Programming/Glava20/index20.htm#z692
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <float.h>
+
+void input_matrix_size(int *n);
+void generate_matrix(int n, float matr[n][n]);
+void print_matrix(int n, float matr[n][n]);
+float find_max(int n, float matr[n][n]);
+void print_result(float max);
+
+int main() {
+    srand(time(0));  // инициализация генератора случайных чисел
+    
+    int n;
+    input_matrix_size(&n);
+    
+    float matr[n][n];
+    generate_matrix(n, matr);
+    
+    print_matrix(n, matr);
+    
+    float max = find_max(n, matr);
+    print_result(max);
+    
+    return 0;
+}
+
+// Функция ввода размера матрицы
+void input_matrix_size(int *n) {
+    printf("Введите n: ");
+    scanf("%d", n);
+}
+
+// Функция генерации матрицы случайными числами от -10 до 10
+void generate_matrix(int n, float matr[n][n]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            matr[i][j] = rand() * 20.0 / RAND_MAX - 10.0;
+            // нельзя rand() / RAND_MAX * 20.0 - 10.0, потому что rand() и RAND_MAX - целые
+        }
+    }
+}
+
+// Функция вывода матрицы на экран
+void print_matrix(int n, float matr[n][n]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%8.2f ", matr[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Функция поиска максимального элемента в заштрихованной области
+float find_max(int n, float matr[n][n]) {
+    float max = -FLT_MAX;                       // минимально возможное значение для float
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i + j < n) {
+                if (matr[i][j] > max)
+                    max = matr[i][j];
+            }
+        }
+    }
+
+    return max;
+}
+
+// Функция вывода результата
+void print_result(float max) {
+    printf("\nНаибольшее значение в заштрихованной части: %.2f", max);
+}
+
+
+
+
 // #include "random_matrix.h"
 // #include <stdio.h>
 // #include <stdlib.h>
@@ -31,47 +108,3 @@
 //     printf("\n%.2f", max);
 //     return 0;
 // }
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <float.h>
-
-void random_float_matrix(int n, int m, float matr[n][m]) { 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            matr[i][j] = rand() * 20.0 / RAND_MAX - 10.0;
-        }
-    }
-}
-
-void print_matrix(int n, int m, float matr[n][m]) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            printf("%8.2f ", matr[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-int main() {
-    srand(time(0));
-    int n;
-    float max = -FLT_MAX;
-    printf("Введите n: ");
-    scanf("%d", &n);
-    float matr[n][n];
-    random_float_matrix(n, n, matr);
-    print_matrix(n, n, matr);
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i + j < n)
-                if (matr[i][j] > max)
-                    max = matr[i][j];
-        }
-    }
-
-    printf("\n%.2f", max);
-    return 0;
-}
